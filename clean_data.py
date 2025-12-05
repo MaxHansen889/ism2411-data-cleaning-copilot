f1b31451e0345e235fdf5f00df964e5fa4f85d34
MaxHansen889 <hansen129@usf.edu>
2025-12-05 15:48:06 -0500

    Update clean_data.py

--- clean_data.py
+++ clean_data.py
@@ -1,8 +1,1 @@
-import pandas as pd
-# Load the raw data
-raw_data = pd.read_csv("sales_raw_data.csv") 
 
-#  Save cleaned version
-raw_data.to_csv("processed_data.csv", index=False)
-
-print("Cleaning complete.")
